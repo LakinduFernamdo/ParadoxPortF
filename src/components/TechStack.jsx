112a253b9eabcc842d85ada2323783ef5d86bedf
2001lakindu <lakindu72001@gmail.com>
2026-03-20 12:37:38 +0530

    add icons

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -22,9 +22,37 @@ const languages = [
 ];
 
 const tools = [
+{
+    name: "Nmap",
+    img: "https://networkwalks.com/wp-content/uploads/2021/06/Nmap-practice-lab1.png",
+  },
+  {
+    name: "Wireshark",
+    img: "https://preview.redd.it/custom-wireshark-icon-because-the-current-one-is-very-bad-v0-dxk87rvwzjmc1.png?auto=webp&s=29952f19fdeb8c85493bdb04265988057bd54e0b",
+  },
+  {
+    name: "Jhon the ripper",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQciD3f2-0VPPTw9NUMShnIAc4eCETtAOmIwg&s",
+  },
+  {
+    name: "Metasploit",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTINUizi44XOMcudPSNTii4W2jHLn_DjnI2UQ&s",
+  },
+  {
+    name: "Aircrack-ng",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkgyz_Rt-LxDHjhX3xDreGk6_4teHSag0_3w&s",
+  },
+  {
+    name: "Beef",
+    img: "https://www.kali.org/tools/beef-xss/images/beef-xss-logo.svg",
+  },
   {
     name: "Spring Boot",
     img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+  },
+  {
+    name: "Express js",
+    img: "https://media.licdn.com/dms/image/v2/D4E12AQHPlbZU5HwV1Q/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1738075363536?e=2147483647&v=beta&t=Cg8zmjgnmbAzdcx52pXwpym4FZyUT1RGrXhc1nPBGao",
   },
   {
     name: "React",
