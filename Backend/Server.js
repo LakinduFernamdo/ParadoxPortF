ade9ed735401c5e06feac624722ceaedf9ea20ae
2001lakindu <lakindu72001@gmail.com>
2026-03-28 09:05:32 +0530

    render can access his port

--- Backend/Server.js
+++ Backend/Server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use("/api/contact", contactRoute);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
